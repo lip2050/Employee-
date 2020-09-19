09e2d2ab4c0650aee28f233316d42111d436d965
lip2050 <lip2050@users.noreply.github.com>
2020-09-19 10:46:53 -0400

    The second assignment for week 2 day 2

---
+++ EmployeeInformation/src/employeeInformation/Employee.java
@@ -0,0 +1,15 @@
+package employeeInformation;
+
+public class Employee {
+	
+	int employeeId;
+	String employeeName;
+	
+	public  Employee(int employeeId, String employeeName) {
+		
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		
+	}
+
+}
---
+++ EmployeeInformation/src/employeeInformation/EmployeeInfo.java
@@ -0,0 +1,11 @@
+package employeeInformation;
+
+public class EmployeeInfo {
+	public static void main(String [] args) {
+		Employee emp = new Employee(223, "Jake");
+		System.out.println("Employee's ID for " + emp.employeeName + " is " + emp.employeeId);
+		
+		
+	}
+
+}
